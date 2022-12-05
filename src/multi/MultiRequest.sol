26cf885c152bb692c383ed41fdfcc4a209baf3dd
kanth <kanth.explorer@gmail.com>
2022-12-05 10:46:34 +0530

    feat: execute multiple BridgeRequest atomically

---
+++ src/multi/MultiRequest.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.8.4;
+
+import "../interfaces/ISocketRegistry.sol";
+import "../utils/Ownable.sol";
+import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MultiRequest is Ownable,ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    /**
+     * @notice Address used to identify if it is a native token transfer or not
+     */
+    address private constant NATIVE_TOKEN_ADDRESS =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /**
+     * @notice variable for our registry contract, registry contract is responsible for redirecting to different bridges
+     */
+    ISocketRegistry public immutable socket;
+
+    constructor(address _socketRegistry, address owner_) Ownable(owner_) {
+        socket = ISocketRegistry(_socketRegistry);
+    }
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+
+    /**
+     * @notice Container for Multiple User Request
+     * @member userRequests requests that is passed in a loop on to the registry
+     */
+    struct MultiRequest {
+        ISocketRegistry.UserRequest[] userRequests;
+    }
+
+    function execute(MultiRequest calldata multiRequest) public payable nonReentrant {
+
+        ISocketRegistry.UserRequest[] memory userRequests = multiRequest.userRequests;
+
+        for (uint i = 0; i < userRequests.length; ++i) {
+            callRegistry(userRequests[i]);
+        }
+    }
+
+    /**
+     * @notice Function that calls the registry after verifying if the fee is correct
+     * @dev multiRequest amount should match the aount after deducting the fee from the input amount
+     * @param _userRequest _userRequest contains the input amount and the bridge request that is passed to socket registry
+     */
+    function callRegistry(ISocketRegistry.UserRequest memory _userRequest) public payable nonReentrant {
+        address inputTokenAddress = _userRequest.bridgeRequest.inputToken;
+        (address routeAddress, , ) = socket.routes(
+                _userRequest.bridgeRequest.id
+            );
+
+        // Call Registry
+        IERC20(inputTokenAddress).safeTransferFrom(msg.sender, address(this), _userRequest.amount);
+
+        IERC20(inputTokenAddress).safeApprove(
+            routeAddress,
+            _userRequest.amount
+        );
+        socket.outboundTransferTo{value: msg.value}(
+            _userRequest
+        );
+    }
+}
